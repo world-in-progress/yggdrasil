9531ef53adeeed2f54831a2629f4bc92045fd113
YcSoku <dsssyc02@gmail.com>
2025-03-08 14:27:47 +0800

    feat(node/tree): make tree can recursively delete nodes from both cache and repository

--- model/model.go
+++ model/model.go
@@ -20,10 +20,26 @@ var basicTypes = map[string]bool{
 	"map":     true,
 }
 
-type ModelManager struct {
-	models map[string]*ModelDefinition
-	mu     sync.RWMutex
-}
+type (
+	FieldDefinition struct {
+		Type     string
+		Required bool
+		Fields   map[string]*FieldDefinition
+		Item     *FieldDefinition
+		Ref      string
+	}
+
+	ModelDefinition struct {
+		Name    string
+		Extends string
+		Fields  map[string]*FieldDefinition
+	}
+
+	ModelManager struct {
+		models map[string]*ModelDefinition
+		mu     sync.RWMutex
+	}
+)
 
 func NewModelManager() (*ModelManager, error) {
 
--- node/tree_test.go
+++ node/tree_test.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/spf13/viper"
 	"github.com/world-in-progress/yggdrasil/db/mongo"
+	"github.com/world-in-progress/yggdrasil/model"
 )
 
 // instance of model BaseNode
@@ -20,22 +21,34 @@ var testChildNode = map[string]any{
 	"time": time.Now().String(),
 }
 
+// instance of model ExtendNode
+var testChildChildNode = map[string]any{
+	"name": "Test Child-Child Node",
+	"time": time.Now().String(),
+}
+
 func TestTree(t *testing.T) {
 	viper.SetConfigName("test_config")
 	viper.SetConfigType("yaml")
 	viper.AddConfigPath("../test")
 
+	// init tree and its dependencies
+	var err error
 	var cacheSize uint = 1 // only one node can be stored in the runtime cache
 	repo := mongo.NewMongoRepository()
-	tree := NewTree("Test tree", repo, cacheSize)
+	modeler, err := model.NewModelManager()
+	if err != nil {
+		t.Fatalf("Failed to create model manager for Tree: %v", err)
+	}
+	tree := NewTree("Test tree", repo, modeler, cacheSize)
 
-	var err error
+	// start test
 	var nodeID string
 	var childID string
 
 	// register test node
 	if nodeID, err = tree.RegisterNode("BaseNode", testNode); err != nil {
-		t.Errorf("%v", err)
+		t.Fatalf("%v", err)
 	}
 
 	// update name of test node
@@ -47,23 +60,53 @@ func TestTree(t *testing.T) {
 	// for its last calltime is earlier than test child node
 	testChildNode["parent"] = nodeID
 	if childID, err = tree.RegisterNode("ExtendNode", testChildNode); err != nil {
-		t.Errorf("%v", err)
+		t.Fatalf("%v", err)
 	}
 
 	// deactivate test child node
 	if err := tree.DeactivateNode(childID); err != nil {
-		t.Errorf("cannot deactivate node: %v", err)
+		t.Fatalf("cannot deactivate node: %v", err)
 	}
 
 	// check if no active node
 	if tree.GetActiveNodeNum() != 0 {
-		t.Errorf("nodes should all be cleaned but not")
+		t.Fatalf("nodes should all be cleaned but not")
+	}
+
+	// register test child-child ndoe
+	testChildChildNode["parent"] = childID
+	if _, err = tree.RegisterNode("ExtendNode", testChildChildNode); err != nil {
+		t.Fatalf("%v", err)
+	}
+
+	// check node record num (expected 3)
+	if recordNum, err := tree.GetNodeRecordNum(); err != nil {
+		t.Fatalf("%v", err)
+	} else if recordNum != 3 {
+		t.Fatalf("node record num is expected to be 3, but is %d", recordNum)
 	}
 
 	// get test node (test node will be auto-activated)
-	if node, err := tree.GetNode(nodeID); err != nil {
-		t.Errorf("%v", err)
+	if node, err := tree.GetNode(childID); err != nil {
+		t.Fatalf("%v", err)
 	} else {
 		fmt.Printf("node children IDs are %v: ", node.GetChildIDs())
+	}
+
+	// delete test node (test child node and test child-child node will be recursively deleted)
+	if err := tree.DeleteNode(nodeID); err != nil {
+		t.Fatalf("%v", err)
+	}
+
+	// check if no active node
+	if tree.GetActiveNodeNum() != 0 {
+		t.Fatalf("nodes should all be deleted but not")
+	}
+
+	// check if all node records are deleted
+	if recordNum, err := tree.GetNodeRecordNum(); err != nil {
+		t.Fatalf("%v", err)
+	} else if recordNum != 0 {
+		t.Fatalf("nodes should all be deleted but not")
 	}
 }
